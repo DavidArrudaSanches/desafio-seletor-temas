d86ac720670f507485517543bb1517028fe4a64b
DavidArrudaSanches <david.sanches2750@gmail.com>
2025-08-19 17:12:52 -0300

    Continuação Projeto. OBS: Esta com erro

--- js/modules/themeLogic.js
+++ js/modules/themeLogic.js
@@ -1,2 +1,18 @@
-// Coletar Variaveis
-const themeSelect = document.querySelector('#theme-switcher')
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+    const themeSwitcher = document.querySelector('.theme-switcher');
+
+    themeSwitcher.addEventListener('click', (event) => {
+        // Pega o botão clicado
+        const clickedButton = event.target;
+        
+        // Verifica se o elemento clicado é um botão com o atributo data-theme
+        if (clickedButton.matches('button[data-theme]')) {
+            // Pega o valor do atributo data-theme do botão
+            const themeName = clickedButton.dataset.theme;
+
+            // Define o atributo data-theme do <body> para o valor do botão
+            body.setAttribute('data-theme', themeName);
+        }
+    });
+});
